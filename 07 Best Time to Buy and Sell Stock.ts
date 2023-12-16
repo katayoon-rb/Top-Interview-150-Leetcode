32de4629449e2f01418ff0a35bfd15eef147d05a
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-16 22:20:53 +0330

    Create 07 Best Time to Buy and Sell Stock.ts

---
+++ 07 Best Time to Buy and Sell Stock.ts
@@ -0,0 +1,16 @@
+/**
+  You are given an array prices where prices[i] is the price of a given stock on the ith day.
+  You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+  Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+**/
+
+function maxProfit(prices: number[]): number {
+    let max = 0
+    let min = prices[0]
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < min) { min = prices[i] }
+        else { max = Math.max( prices[i] - min, max ) }
+    }
+    return max
+}

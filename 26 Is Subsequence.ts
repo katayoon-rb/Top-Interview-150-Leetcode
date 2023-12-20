2326a364c1082e6a1d34c91f90a48b449759738c
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-20 12:07:58 +0330

    Create 26 Is Subsequence.ts

---
+++ 26 Is Subsequence.ts
@@ -0,0 +1,16 @@
+/**
+  Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+  
+  A subsequence of a string is a new string that is formed from the original string by deleting some
+  (can be none) of the characters without disturbing the relative positions of the remaining characters.
+  (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+*/
+
+function isSubsequence(s: string, t: string): boolean {
+    let i = 0
+
+    for (let j = 0; j < t.length; j++) {
+        if (t.charAt(j) === s.charAt(i)) { i++ }
+    }
+    return i === s.length
+}

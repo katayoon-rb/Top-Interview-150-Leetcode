35a1c478bc5d92299674e4a3fe88037ce1101f18
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-21 11:25:13 +0330

    Update 32 Substring with Concatenation of All Words.ts

--- 32 Substring with Concatenation of All Words.ts
+++ 32 Substring with Concatenation of All Words.ts
@@ -11,16 +11,38 @@
 
 
 function findSubstring(s: string, words: string[]): number[] {
-    words.sort()
-    let right = words.reduce((acc, curr) => acc += curr.length, 0)
+  if (s.length == 0 || words.length == 0) {
+    return []
+  }
 
-    const results = []
-    for (let i = 0 ; right < s.length + 1 ; i++, right++) {
-        let subString = s.substring( i, right ).match( new RegExp(`.{1,${words[0].length}}`, 'g') ).sort()
-        if (JSON.stringify(subString) === JSON.stringify(words)) {
-            results.push(i)
-        }
+  let wordCount = new Map()
+  for (const word of words) {
+    wordCount.set(word, (wordCount.get(word) || 0) + 1)
+  }
+
+  const n = words.length
+  const len = words[0].length
+  let res: number[] = []
+
+  for (let i = 0; i <= s.length - n * len; i++) {
+    let strCount = new Map()
+    let j = 0
+
+    while (j < n) {
+      const start = i + j * len
+      const end = start + len
+      const t = s.substring(start, end)
+
+      if (!wordCount.get(t)) { break }
+      strCount.set(t, (strCount.get(t) || 0) + 1)
+
+      if (strCount.get(t) > wordCount.get(t)) { break }
+      j += 1
     }
 
+    if (j == n) { res.push(i) }
+  }
+  return res
+}
     return results
 }

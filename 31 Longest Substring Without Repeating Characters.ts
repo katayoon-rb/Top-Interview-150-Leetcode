db2fc922b20239ed3c9813794c84969f7f589eff
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-21 11:17:03 +0330

    Create 31 Longest Substring Without Repeating Characters.ts

---
+++ 31 Longest Substring Without Repeating Characters.ts
@@ -0,0 +1,20 @@
+/**
+  Given a string s, find the length of the longest substring
+  without repeating characters.
+*/
+
+
+function lengthOfLongestSubstring(s: string): number {
+  const scanner: string[] = []
+  let longest = 0
+
+  for (const char of s) {
+    const possibleIndex = scanner.indexOf(char)
+
+    if (possibleIndex !== -1) { scanner.splice( 0, possibleIndex + 1 ) }
+    scanner.push(char)
+    longest = Math.max( longest, scanner.length )
+  }
+
+  return longest
+}

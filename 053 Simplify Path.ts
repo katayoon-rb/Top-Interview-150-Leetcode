b9186d22e9bad74e1ee2a853d2438c2fb812aed4
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-26 15:39:50 +0330

    Create 053 Simplify Path.ts

---
+++ 053 Simplify Path.ts
@@ -0,0 +1,29 @@
+/**
+  Given a string path, which is an absolute path (starting with a slash '/')
+  to a file or directory in a Unix-style file system, convert it to the simplified canonical path.
+  
+  In a Unix-style file system, a period '.' refers to the current directory, a double period '..' refers
+  to the directory up a level, and any multiple consecutive slashes (i.e. '//') are treated as a single
+  slash '/'. For this problem, any other format of periods such as '...' are treated as file/directory names.
+  
+  The canonical path should have the following format:
+      The path starts with a single slash '/'.
+      Any two directories are separated by a single slash '/'.
+      The path does not end with a trailing '/'.
+      The path only contains the directories on the path from the root directory to the target file or directory (i.e., no period '.' or double period '..')
+  
+  Return the simplified canonical path.
+*/
+
+
+function simplifyPath(path: string): string {
+    const stack = []
+    const arr = path.split('/').filter(Boolean)
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '..') { stack.pop() }
+        else if (arr[i] === '.') { continue }
+        else { stack.push( arr[i] ) }
+    }
+    return `/${stack.join('/')}`
+}
